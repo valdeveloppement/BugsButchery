d1cad6c7bab7b79db91307e20165707816a51ae6
maura <maura.danloy@gmail.com>
2019-10-14 17:01:01 +0200

    method maurane

--- src/main/java/com/bugsButchery/demo/Player.java
+++ src/main/java/com/bugsButchery/demo/Player.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 public class Player {
 
+	//---- value objet ----//
 	private int playerId;
 	private String playerName;
 	private String playerAntsBreed;
 	private ArrayList<Territory> playerTerritoryList; 
-	private ArrayList<Territory> playerTerritoryFamilyList;
+	private ArrayList<Family> playerTerritoryFamilyList;
 	private int playerAvailableAnts;
 	
+	//---- constructor ----//
 	public Player() {
 		super();
 	}
 	
+	//---- get & set ----//
 	public int getPlayerId() {
 		return playerId;
 	}
@@ -39,10 +42,10 @@ public class Player {
 	public void setPlayerTerritoryList(ArrayList<Territory> playerTerritoryList) {
 		this.playerTerritoryList = playerTerritoryList;
 	}
-	public ArrayList<Territory> getPlayerTerritoryFamilyList() {
+	public ArrayList<Family> getPlayerTerritoryFamilyList() {
 		return playerTerritoryFamilyList;
 	}
-	public void setPlayerTerritoryFamilyList(ArrayList<Territory> playerTerritoryFamilyList) {
+	public void setPlayerTerritoryFamilyList(ArrayList<Family> playerTerritoryFamilyList) {
 		this.playerTerritoryFamilyList = playerTerritoryFamilyList;
 	}
 	public int getPlayerAvailableAnts() {
@@ -64,5 +67,43 @@ public class Player {
 //		
 //	}
 	
+	
+	/**
+	 * placer le nombre de fourmis que l'on veut sur un territoire possédé
+	 * @param player
+	 * @param territory
+	 * @param ants
+	 * @return
+	 */
+	public ArrayList<Territory> placeAnts(Player player, Territory territory, int ants) {
+		
+		if (player.getPlayerTerritoryList().contains(territory)) {
+		//si le player possède le territoire (nommé ici territory) qu'on fait passer dans la méthode	
+			territory.setTerritoryAntsNb(territory.getTerritoryAntsNb()+ants);
+			//le territoire possédé ...
+			playerAvailableAnts = playerAvailableAnts - ants;
+		}
+		return playerTerritoryList;
+		//retourn la liste des territoires qui on changé dans la methode
+	}
+
+	/**
+	 * placer tour a tour une fourmi pour définir a qui sont les territoires
+	 * @param player
+	 * @param territory
+	 * @return
+	 */
+	public ArrayList<Territory> placeFirstAnts(Player player, Territory territory) {
+		
+		if (territory.getTerritoryOwner() == null) {
+		//si le territoire séléctionner est égal a vide
+			playerTerritoryList.add(territory);
+			//ajoute territoire a la liste de territoire du player
+			playerAvailableAnts = playerAvailableAnts - 1;
+			//enlever une fourmi au compte total de fourmi du player
+		}
+		return playerTerritoryList;
+		//retourn la liste des territoires qui on changé dans la methode
+	}
 	
 }
