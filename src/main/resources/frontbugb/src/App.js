64784a58f2d0ad7a845371fcb8673b98d0d59fcc
maura <maura.danloy@gmail.com>
2019-10-22 15:27:03 +0200

    front-end start

--- src/main/resources/frontbugb/src/App.js
+++ src/main/resources/frontbugb/src/App.js
@@ -42,28 +42,35 @@ alert(`${this.value}`)
     } 
 
    return (
-<div> 
-<Infos value="players" info="tous les players"/>
-<Infos value="info" info="infos générales"/>
+<div className="contenant"> 
 
-<div className="carte">
-<Territory action={this.alert} color="epinards" value="épinards" int="" player="" family="légume"/>
-<Territory action={this.alert} color="framboise" value="framboise" int="" player="" family="fruit"/>
-<Territory action={this.alert} color="kiwi" value="kiwi" int="" player="" family="fruit"/>
-<Territory action={this.alert} color="foie_gras" value="foie gras" int="" player="" family="viande"/>
-<Territory action={this.alert} color="aubergine" value="aubergine" int="" player="" family="légumes"/>
-<Territory action={this.alert} color="jambon" value="jambon" int="" player="" family="fruit"/>
-<Territory action={this.alert} color="poivron" value="poivron" int="" player="" family="légume"/>
-<Territory action={this.alert} color="saucisse" value="saucisse" int="" player="" family="viande"/>
-<Territory action={this.alert} color="abricot" value="abricot" int="" player="" family="fruit"/>
-</div>
-<Button action={this.attack} value="attack" />
-<Button action={this.move} value="move"/>
-<Button action={this.suivant} value="Suivant" />
-{button}
+  <div className="carte">
+    <Territory action={this.alert} color="epinards" value="épinards" int="" player="" family="légume"/>
+    <Territory action={this.alert} color="framboise" value="framboise" int="" player="" family="fruit"/>
+    <Territory action={this.alert} color="kiwi" value="kiwi" int="" player="" family="fruit"/>
+    <Territory action={this.alert} color="foie_gras" value="foie gras" int="" player="" family="viande"/>
+    <Territory action={this.alert} color="aubergine" value="aubergine" int="" player="" family="légumes"/>
+    <Territory action={this.alert} color="jambon" value="jambon" int="" player="" family="viande"/>
+    <Territory action={this.alert} color="poivron" value="poivron" int="" player="" family="légume"/>
+    <Territory action={this.alert} color="saucisse" value="saucisse" int="" player="" family="viande"/>
+    <Territory action={this.alert} color="abricot" value="abricot" int="" player="" family="fruit"/>
+  </div>
+
+  <div className="informationJeu">
+    <Infos value="players" info="tous les players"/>
+    <Infos value="info" info="infos générales"/>
+  </div>
+
+  <div>
+    <Button action={this.attack} value="attack" />
+    <Button action={this.move} value="move"/>
+    <Button action={this.suivant} value="Suivant" />
+    {button}
+  </div>
+  
 </div>
     )
   }
 }
 
-export default App;
+export default App
