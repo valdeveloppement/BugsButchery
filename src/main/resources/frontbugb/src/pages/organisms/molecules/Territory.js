3a55e254301ce70b33d045029a46a1fa00360d5a
maura <maura.danloy@gmail.com>
2019-10-29 14:11:16 +0100

    recup debug

--- src/main/resources/frontbugb/src/pages/organisms/molecules/Territory.js
+++ src/main/resources/frontbugb/src/pages/organisms/molecules/Territory.js
@@ -1,16 +1,25 @@
-import React from 'react';
-
+import React from "react";
 
 class Territory extends React.Component {
-    
-
-    render() {
-        return (
-            <div onClick={() => { this.props.action() }} className="territory" id={this.props.color}>
-                {this.props.value + " Famille : " + this.props.family.familyName + ", Propriétaire : " + this.props.player + ", Nombre de fourmis sur le territoire : " + this.props.int}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        onClick={() => {
+          this.props.action();
+        }}
+        className="territory"
+        id={this.props.color}
+      >
+        {this.props.value +
+          " Famille : " +
+          this.props.family.familyName +
+          ", Propriétaire : " +
+          this.props.player +
+          ", Nombre de fourmis sur le territoire : " +
+          this.props.int}
+      </div>
+    );
+  }
 }
 
 export default Territory;
